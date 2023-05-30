30de8e122006ce89de6bbdcd7617d895e28ca214
anggerha <angger.herlambang@ti.ukdw.ac.id>
2023-05-30 12:26:04 +0700

    minor update

--- homepage/src/components/Profile.vue
+++ homepage/src/components/Profile.vue
@@ -29,7 +29,7 @@
                 </div>
                 <div class="mb-6" v-if="dataDiri.username_telegram || dataDiri.username_telegram =='' && dataDiri.role == 'MAHASISWA'">
                     <label for="Id Telegram" class="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Username Telegram</label>
-                    <input v-model="dataDiri.username_telegram" type="text" id="username_telegram" class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
+                    <input v-model="dataDiri.username_telegram" type="text" id="username_telegram" class="bg-gray-300 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" disabled required/>
                 </div>
                 <div class="mb-6" v-if="dataDiri.id_telegram || dataDiri.id_telegram == '' && dataDiri.role == 'MAHASISWA' ">
                     <label for="Id Telegram" class="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Chat ID Telegram</label>
