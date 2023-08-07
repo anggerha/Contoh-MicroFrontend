913a7849f2bb9d4274dbea9fb83176e4a106f30a
anggerha <angger.herlambang@ti.ukdw.ac.id>
2023-08-07 12:15:40 +0700

    Update Form

--- srm/src/components/FormData.vue
+++ srm/src/components/FormData.vue
@@ -206,9 +206,70 @@
                 <b-button class="btn" @click="submit" variant="primary">Submit</b-button>
                 <b-button class="btn" @click="reset" variant="danger">Reset</b-button>
             </div>
+<!-- Informasi Umum -->
+            <div v-if="form.jenis_jadwal == 'informasi_umum'">
+                <b-form-group id="input-group-2" label="Topik Informasi Umum" label-for="input-2">
+                    <b-form-select
+                    id="input-2"
+                    v-model="form.topik_jadwal"
+                    :options="optionTopik"
+                    required
+                    ></b-form-select>
+                </b-form-group>
+
+                <b-form-group
+                    id="input-group-3"
+                    label="Pertanyaan"
+                    label-for="input-3"
+                >
+                    <b-form-input
+                    id="input-3"
+                    v-model="form.nama_kegiatan"
+                    type="text"
+                    placeholder="Pertanyaan"
+                    required
+                    ></b-form-input>
+                </b-form-group>
+
+                <b-form-group
+                    id="input-group-4"
+                    label="Jawaban"
+                    label-for="input-4"
+                >
+                    <b-form-textarea
+                    id="textarea"
+                    placeholder="Jawaban"
+                    rows="3"
+                    max-rows="6"
+                ></b-form-textarea>
+                </b-form-group>
+
+                <!-- <b-form-group id="input-group-4" label="Tanggal Mulai" label-for="input-4">
+                    <b-form-input
+                    id="input-4"
+                    v-model="form.tanggal_mulai"
+                    type="datetime-local"
+                    placeholder="Tanggal Mulai"
+                    required
+                    ></b-form-input>
+                </b-form-group>
+
+                <b-form-group id="input-group-5" label="Tanggal Selesai" label-for="input-5">
+                    <b-form-input
+                    id="input-5"
+                    v-model="form.tanggal_selesai"
+                    type="datetime-local"
+                    placeholder="Tanggal Selesai"
+                    required
+                    ></b-form-input>
+                </b-form-group> -->
+
+                <b-button class="btn" @click="submit" variant="primary">Submit</b-button>
+                <b-button class="btn" @click="reset" variant="danger">Reset</b-button>
+            </div>
         </b-form>
         <div>
-            <b-row>
+            <b-row class="list">
                 <b-col cols="12" md="4" lg="3" xl="3">
                     <h5 class="judul-kategori">List Jadwal Khusus</h5>
                     <b-row v-if="jumlahPageJadwalKhusus !=null" class="pagination">
@@ -348,6 +409,37 @@
                         </div>
                     </div>
                 </b-col>
+                <!-- list informasi umum -->
+                <!-- <b-col cols="12">
+                    <h5 class="judul-kategori">List Informasi Umum</h5>
+                    <b-row v-if="jumlahPageJadwalPembayaran !=null" class="pagination">
+                        <b-col>
+                            <b-button class="page" id="prev" :disabled="pageJadwalPembayaran <=1" @click="pageJadwalPembayaran -=1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+                                </svg>
+                            </b-button>
+                        </b-col>
+                        <b-col style="text-align:center; margin:auto;">
+                            {{pageJadwalPembayaran}}/{{Math.ceil(jumlahPageJadwalPembayaran)}}
+                        </b-col>
+                        <b-col>
+                            <b-button class="page" id="next" :disabled="pageJadwalPembayaran >= jumlahPageJadwalPembayaran" @click="pageJadwalPembayaran +=1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+                                </svg>
+                            </b-button>
+                        </b-col>
+                    </b-row>
+                    <div v-for="item in listJadwalPembayaran" :key="item._id" class="listCard shadow p-3 mb-2 bg-white rounded">
+                        <div class="isiList" >   
+                            <h6>{{item.nama_kegiatan}}</h6>
+                            <p><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-calendar3" viewBox="0 0 16 16">
+                                <path d="M14 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM1 3.857C1 3.384 1.448 3 2 3h12c.552 0 1 .384 1 .857v10.286c0 .473-.448.857-1 .857H2c-.552 0-1-.384-1-.857V3.857z"/>
+                                <path d="M6.5 7a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
+                                </svg> {{item.tanggal_mulai}} s.d {{item.tanggal_selesai}}
+                            </p>
+                        </div>
+                    </div>
+                </b-col> -->
             </b-row>
         </div>
     </div>
@@ -355,6 +447,7 @@
 
 <script>
 import axios from 'axios';
+import moment from 'moment';
 
 export default {
     name: 'FormData',
@@ -369,6 +462,8 @@ export default {
                 tanggal_mulai: '',
                 tanggal_selesai: '',
                 attachment: null,
+                // pertanyaan: '',
+                // jawaban: ''
             },
             optionKlasifikasi: [
                 { text: 'Pilih Jenis Jadwal', value: null, disabled: true },
@@ -376,6 +471,7 @@ export default {
                 { text: 'Jadwal Khusus', value: 'jadwal_khusus' },
                 { text: 'Jadwal Ujian', value: 'jadwal_ujian' },
                 { text: 'Pembayaran', value: 'pembayaran' },
+                // { text: 'Informasi Umum', value: 'informasi_umum' }
             ],
             optionTopik: [],
             listMatkul: [],
@@ -402,10 +498,6 @@ export default {
             this.getListJadwalPembayaran()
             this.getListJadwalUjian()
             this.getListJadwalUjianMatkul()
-            
-            
-           
-           
         }
     },
     methods: {
@@ -428,6 +520,10 @@ export default {
             try {
                 await axios.get(`https://beritaapi.fti.ukdw.ac.id/admin/${this.firebaseUID.uid}/jadwal-ujian`).then((response)=>{
                     this.listJadwalUjian = response.data.reverse()
+                    for(var i=0; i < this.listJadwalUjian.length; i++){
+                        this.listJadwalUjian[i].tanggal_mulai = moment(this.listJadwalUjian[i].tanggal_mulai).locale('id').format("DD-MM-YYYY")
+                        this.listJadwalUjian[i].tanggal_selesai = moment(this.listJadwalUjian[i].tanggal_selesai).locale('id').format("DD-MM-YYYY")
+                    }
                     this.jumlahPageJadwalUjian = this.listJadwalUjian.length/5
                     this.pageJadwalUjian = 1
                 })
@@ -441,7 +537,7 @@ export default {
                     this.listJadwalUjianMatkul = response.data.reverse()
                     this.jumlahPageJadwalUjianMatkul = this.listJadwalUjianMatkul.length/5
                     this.pageJadwalUjianMatkul = 1
-                    console.log(response.data);
+                    // console.log(response.data);
                 })
             } catch (error) {
                 console.log(error);
@@ -451,9 +547,13 @@ export default {
             try {
                 await axios.get(`https://beritaapi.fti.ukdw.ac.id/admin/${this.firebaseUID.uid}/jadwal-khusus`).then((response)=>{
                     this.listJadwalKhusus = response.data.reverse()
+                    for(var i=0; i < this.listJadwalKhusus.length; i++){
+                        this.listJadwalKhusus[i].tanggal_mulai = moment(this.listJadwalKhusus[i].tanggal_mulai).locale('id').format("DD-MM-YYYY")
+                        this.listJadwalKhusus[i].tanggal_selesai = moment(this.listJadwalKhusus[i].tanggal_selesai).locale('id').format("DD-MM-YYYY")
+                    }
                     this.jumlahPageJadwalKhusus = this.listJadwalKhusus.length/5
                     this.pageJadwalKhusus = 1
-                    console.log(this.listJadwalKhusus);
+                    // console.log(this.listJadwalKhusus);
                 })
             } catch (error) {
                 console.log(error);
@@ -479,7 +579,7 @@ export default {
                         position: 'top'
                     });
                     if(this.form.jenis_jadwal == 'jadwal_ujian_mata_kuliah'){
-                        console.log(response);
+                        // console.log(response);
                         this.getListJadwalUjianMatkul()
                     }else if(this.form.jenis_jadwal == 'jadwal_ujian'){
                         this.getListJadwalUjian()
@@ -547,7 +647,16 @@ export default {
                     { text: 'Pendadaran', value: 'pendadaran' },
                     { text: 'Wisuda', value: 'wisuda' }
                 ]
-            }
+            } 
+            // else if (this.form.jenis_jadwal == 'informasi_umum') {
+            //     this.optionTopik = [
+            //         { text: 'Pilih Jenis Informasi Umum', value: null, disabled: true },
+            //         { text: 'Informatika', value: 'informatika' },
+            //         { text: 'Sistem Informasi', value: 'sistem_informasi' },
+            //         { text: 'UKDW', value: 'ukdw' },
+            //         { text: 'Beasiswa', value: 'beasiswa' }        
+            //     ]
+            // }
         },
         namaKegiatan() {
             if(this.form.topik_jadwal != '' && this.form.topik_jadwal != null && this.form.selected != null){
@@ -566,6 +675,9 @@ export default {
 .canvas{
     @media only screen and (max-width: 600px) {
         margin: 0 1rem;
+        .col-12{
+            margin-bottom: 2rem;
+        }
     }
     margin: 0 4rem;
 }
@@ -624,5 +736,8 @@ button#next{
     color: white !important;
     background-color: #32a3df;
     border: none;
+}
+.list{
+    margin-top: 2rem;
 }
 </style>
