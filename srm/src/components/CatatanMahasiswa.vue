3a90c114916513c4ddb9a1ffbbee5e1a19c688a3
anggerha <angger.herlambang@ti.ukdw.ac.id>
2023-10-09 18:57:51 +0700

    Update: Remove 'tidak_adktif' Status

--- srm/src/components/CatatanMahasiswa.vue
+++ srm/src/components/CatatanMahasiswa.vue
@@ -66,7 +66,6 @@
                                             <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm4.5 0a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zM8 11a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm5 2.755C12.146 12.825 10.623 12 8 12s-4.146.826-5 1.755V14a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-.245z"/>
                                         </svg>
                                         <span v-if="dataPerwalian.status == 'aktif' " style="margin:0 0.5rem; "><h5 class="mt-1" style="text-align: center;"><b-badge variant="success">Aktif</b-badge></h5></span>
-                                        <span v-if="dataPerwalian.status == 'tidak_aktif' " style="margin:0 0.5rem; "><h5 class="mt-1" style="text-align: center;"><b-badge variant="warning">Tidak Aktif</b-badge></h5></span>
                                         <span v-if="dataPerwalian.status == 'alumni' " style="margin:0 0.5rem; "><h5 class="mt-1" style="text-align: center;"><b-badge variant="primary">Alumni</b-badge></h5></span>
                                     </div>
                                     
